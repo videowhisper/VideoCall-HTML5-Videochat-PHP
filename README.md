d5bb180a76a139563363be3e2b44c5317fffb87d
VideoWhisper.com <videowhispers@gmail.com>
2020-09-17 13:30:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# PHP-HTML5-Videochat / Video Call
+
+[PHP HTML5 Videochat / Video Call](https://demo.videowhisper.com/videocall-html5-videochat-php//)
+
+![PHP HTML5 Videochat / Video Call](/snapshots/h5a-2w-c.png)
+
+
+Before installing, test the simple setup in the live demo:
+[PHP HTML5 Videochat / Video Call : Live Demo](https://demo.videowhisper.com/videocall-html5-videochat-php/)
+
+This plain php edition includes code and minimal scripts tp embed a HTML5 Videochat app and test/showcase some features. This edition is for integrating/using application with own scripts/framework.
+This edition showcases 2 way videocalls.
+For a complete implementation of advanced capabilities, see [Turnkey HTML5 Videochat Site](https://paidvideochat.com/html5-videochat/) edition, available as WordPress plugin with full php source. The turnkey site edition implements pay per minute videochat (group and private 2 way video calls) with membership, billing, advanced tools.
+
+## Simple PHP Edition Features: Video Call
+ * Automatically create a room as caller and show link to invite client
+ * Broadcast and playback live video at same time using HTML5 WebRTC
+ * Simple implementation of signaling broadcast (to connect automatically) and text chat, using plain text files
+ 
+
+##  Key Features for HTML5 Videochat / Video Call
+Warning: some of these features are not active/implemented in this simplified edition, but can be enabled as in turnkey site edition.
+ * 2 way video call
+ * audio recorder, emoticons, mentions in text chat
+ * WebRTC relayed streaming (reliable and scalable to many clients from streaming server, independent of broadcaster upload connection)
+ * fullscreen for videochat interface or playback video
+ * adaptive target video bitrate (depending on cam resolution) and configuration in resolution change
+ * broadcasting/playback stats (open controls and stats should show in few seconds)
+ * dark mode / lights on: each user can toggle interface mode live at runtime
+ * Sfx (sound effects)
+ * live wallet balance display (updates from tips and other transfers)
+ * tips with multiple customizable options, gift images
+ * translation support
+
+## Installation Instructions
+ * Before installing, make sure your hosting environment meets all [requirements](https://videowhisper.com/?p=Requirements) including for a HTML5 WebRTC streaming relay. Production implementations should also involve Session Control. 
+  
+ 1. If you don't use a [turnkey webrtc relay streaming hosting](https://webrtchost.com/hosting-plans/), configure WebRTC and SSL with Wowza SE.
+ 2. Deploy files to your web installation location. (Example: yoursite.domain/html5-videochat/)
+ 3. Fill your streaming settings in settings.php file
+ 4. If you don't have SuPHP, enable write permissions (0777) for folder "uploads", required to save session and chat info.
+
+## Plain PHP Edition Limitations
+ * The plain php edition refers to minimal scripts for configuring and accessing videochat room, so developers can integrate with own scripts. 
+ * Plain php edition does not involve database and systems to manage members, rooms, billing. These depend on framework you want to integrate, plugins, database, member system. 
+ * Applications reads parameters, wallet balance and other data with ajax calls from framework/integration scripts (that need to be implemented depending on framework, database, user scripts).
+ * A complete implementation of features is available for WordPress framework. See [Turnkey HTML5 Videochat Site](https://paidvideochat.com/html5-videochat/) edition, available as WordPress plugin with full php source. Includes user role management (performers/clients), pay per minute, integrates billing wallets.
+ * This plain edition implements 2 way videocall with broadcast / playback.
+ 
+## Main Integration Scripts
+ * index.php embeds the html5 application: accessed directly creates a room and shows room link to invite others
+ * app-call.php is called by application to retrieve parameters, interact with web server, update status and chat (ajax calls)
+ * app-functions.php functions implementing features for app-call.php , including translated texts, app settings
+ * settings.php settings and options, including streaming settings and url for calls (when integrating with own framework)
+
+Scripts also contain comments for clarifications/suggestions. 
+
+This is a simple setup showcasing easy app deployment and integration with other PHP scripts. 
+For a quick setup, see [VideoWhisper Turnkey Stream Hosting Plans](https://webrtchost.com/hosting-plans/) that include requirements for all features and free installation.
+
+For assistance and clarifications, [Contact VideoWhisper Support](https://videowhisper.com/tickets_submit.php).
+
+
+For a more advanced setup, see this turnkey site solution based on WP: 
+[HTML5 Paid Videochat Site Software](https://paidvideochat.com/html5-videochat/) 
+
---
+++ app-functions.php
@@ -0,0 +1,284 @@
+<?php
+//app functions needed by app calls, depend on integration
+
+//For more advanced implementations see WordPress plugin and its php source code:
+//https://wordpress.org/plugins/ppv-live-webcams/
+//https://plugins.svn.wordpress.org/ppv-live-webcams/trunk/inc/h5videochat.php 
+
+
+//demo setup saves variables in plain files in uploads folder: integration should use framework database
+
+		function varSave($path, $var)
+		{
+			if (!file_exists('uploads')) mkdir('uploads');
+			file_put_contents('uploads/' . $path, serialize($var));
+		}
+
+		function varLoad($path)
+		{
+			if (!file_exists('uploads/' . $path)) return false;
+
+			return unserialize(file_get_contents('uploads/' . $path));
+		}
+		
+		function arrayLoad($path)
+		{
+			$res = varLoad($path);
+			
+			if (is_array($res)) return $res;
+			else return array();
+		}
+		
+// app parameter functions
+
+	function __(  $text,  $domain = 'default' )
+	{
+		return 	$text;
+	}
+
+		 function path2url($file)
+		{
+			$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+			return dirname($url) . '/' . str_replace( dirname(__FILE__) , '', $file);
+		}
+		
+	 function appFail($message = 'Request Failed', $response = null)
+	{
+		//bad request: fail
+		
+		if (!$response) $response = array();
+
+		$response['error'] = $message;
+
+		$response['VideoWhisper'] = 'https://videowhisper.com';
+
+		echo json_encode($response);
+
+		die();
+	}
+	
+	
+	 function appSfx()
+	{
+		//sound effects sources
+		
+		$base = VW_H5V_URL. 'sounds/';
+		
+		
+		return array(
+		'message' => $base . 'message.mp3',
+		'hello' => $base . 'hello.mp3',
+		'leave' => $base . 'leave.mp3',	
+		'call' => $base . 'call.mp3',
+		'warning' => $base . 'warning.mp3',
+		'error' => $base . 'error.mp3',
+		'buzz' => $base . 'buzz.mp3',
+		);
+	}
+		
+	function appText()
+	{
+		//implement translations
+
+		//returns texts
+			return array(
+			'Send' => __('Send', 'ppv-live-webcams'),
+			'Type your message' => __('Type your message', 'ppv-live-webcams'),
+
+			'Chat' => __('Chat', 'ppv-live-webcams'),
+			'Camera' => __('Camera', 'ppv-live-webcams'),
+			'Users' => __('Users', 'ppv-live-webcams'),
+			'Options' => __('Options', 'ppv-live-webcams'),
+			'Files' => __('Files', 'ppv-live-webcams'),
+			'Presentation' => __('Presentation', 'ppv-live-webcams'),
+
+			'Tap for Sound' => __('Tap for Sound', 'ppv-live-webcams'),
+			'Enable Audio' => __('Enable Audio', 'ppv-live-webcams'),
+			'Mute' => __('Mute', 'ppv-live-webcams'),
+			'Reload' => __('Reload', 'ppv-live-webcams'),
+
+			'Broadcast' => __('Broadcast', 'ppv-live-webcams'),
+			'Stop Broadcast' => __('Stop Broadcast', 'ppv-live-webcams'),
+			'Make a selection to start!' => __('Make a selection to start!', 'ppv-live-webcams'),
+
+			'Lights On' => __('Lights On', 'ppv-live-webcams'),
+			'Dark Mode' => __('Dark Mode', 'ppv-live-webcams'),
+			'Enter Fullscreen' => __('Enter Fullscreen', 'ppv-live-webcams'),
+			'Exit Fullscreen' => __('Exit Fullscreen', 'ppv-live-webcams'),
+
+			'Site Menu' => __('Site Menu', 'ppv-live-webcams'),
+
+			'Request Private' => __('Request Private', 'ppv-live-webcams'),
+			'Request Private 2 Way Videochat Show' => __('Request Private 2 Way Videochat Show', 'ppv-live-webcams'),
+			'Performer Disabled Private Requests' => __('Performer Disabled Private Requests', 'ppv-live-webcams'),
+			'Performer is Busy in Private' => __('Performer is Busy in Private', 'ppv-live-webcams'),
+			'Performer is Not Online' => __('Performer is Not Online', 'ppv-live-webcams'),
+			'Nevermind' => __('Nevermind', 'ppv-live-webcams'),
+			'Accept' => __('Accept', 'ppv-live-webcams'),
+			'Decline' => __('Decline', 'ppv-live-webcams'),
+			'Close Private' => __('Close Private', 'ppv-live-webcams'),
+
+			'Next' => __('Next', 'ppv-live-webcams'),
+			'Next: Random Videochat Room' => __('Next: Random Videochat Room', 'ppv-live-webcams'),
+
+			'Name' => __('Name', 'ppv-live-webcams'),
+			'Size' => __('Size', 'ppv-live-webcams'),
+			'Age' => __('Age', 'ppv-live-webcams'),
+			'Upload: Drag and drop files here, or click to select files' =>  __('Upload: Drag and drop files here, or click to select files', 'ppv-live-webcams'),
+			'Uploading. Please wait...' =>  __('Uploading. Please wait...', 'ppv-live-webcams'),
+			'Open' => __('Open', 'ppv-live-webcams'),
+			'Delete' => __('Delete', 'ppv-live-webcams'),
+
+			'Media Displayed' => __('Media Displayed', 'ppv-live-webcams'),
+			'Remove' => __('Remove', 'ppv-live-webcams'),
+			'Default' => __('Default', 'ppv-live-webcams'),
+			'Empty' => __('Empty', 'ppv-live-webcams'),
+
+			'Profile' => __('Profile', 'ppv-live-webcams'),
+			'Show' => __('Show', 'ppv-live-webcams'),
+
+			'Private Call' => __('Private Call', 'ppv-live-webcams'),
+			'Exit' => __('Exit', 'ppv-live-webcams'),
+
+			'External Broadcast' => __('External Broadcast', 'ppv-live-webcams'),
+			'Broadcast with external apps for advanced compositions, scenes, effects or reliability compared to web based interface and protocols. External broadcasts have higher latency and improved capacity, reliability specific to HLS delivery method. New broadcasts show in about 10 seconds and unavailability updates after 1 minute.' => __('Broadcast with external apps for advanced compositions, scenes, effects or reliability compared to web based interface and protocols. External broadcasts have higher latency and improved capacity, reliability specific to HLS delivery method. New broadcasts show in about 10 seconds and unavailability updates after 1 minute.', 'ppv-live-webcams'),
+		);
+	}
+
+
+function appRoomUsers($roomID, $options)
+	{
+		
+		$sessions = arrayLoad($roomID . '_sessions');
+		
+		foreach ($sessions as $key => $session)
+			{
+				if (!is_array($userMeta = unserialize($session['meta']))) $userMeta = array();
+
+				$item = [];
+				$item['userID'] = intval($session['uid']);
+				$item['userName'] = $session['username'];
+				if (!$item['userName']) $item['userName'] = '#' . $session['uid'];
+				
+				$item['sdate'] = intval($session['sdate']);
+				$item['meta'] = $userMeta;
+				$item['updated'] = intval($session['edate']);
+				$item['avatar'] =  VW_H5V_URL .'images/avatar.png';
+				$item['url'] = 'https://videowhisper.com/tickets_submit.php';
+
+				$items[intval($session['uid'])] = $item;
+			}
+			
+			return $items;
+	}
+
+	
+ function appTipOptions($options)
+	{
+
+		$tipOptions = stripslashes($options['tipOptions']);
+		if ($tipOptions)
+		{
+			$p = xml_parser_create();
+			xml_parse_into_struct($p, trim($tipOptions), $vals, $index);
+			$error = xml_get_error_code($p);
+			xml_parser_free($p);
+
+			if (is_array($vals)) return $vals;
+		}
+
+		return array();
+
+	}
+
+
+function appStream($userID, $roomID, $options)
+{
+	$key = $options['webKey'] . $roomID; //a secret key to implement stream verification
+
+	return 'stream' . $userID . '?channel_id=' . $roomID . '&userID=' . urlencode($userID) . '&key=' . urlencode($key) . '&ip=' . urlencode($_SERVER['REMOTE_ADDR']) . '&transcoding=0&room=Room' . $roomID. '&privateUID=0';
+
+}
+
+function appPublicRoom($roomID, $userID, $options, $welcome ='')
+{
+	//public room parameters, specific for this user
+	//depends on integration
+
+	$room = array();
+
+	$room['ID'] = $roomID;
+	$room['name'] = 'Room' . $roomID;
+
+	$room['performer'] = 'Caller' . (10000+$roomID);
+	$room['performerID'] = (10000+$roomID);
+
+	$collaboration = $options['collaboration'];
+	if (VW_DEVMODE && VW_DEVMODE_COLLABORATION) $collaboration = true;
+
+	$isPerformer = ($userID == (10000+$roomID));
+
+	$clientID = $roomID;
+	
+	// $privateUID = the other user ID
+	
+	if ($isPerformer) $privateUID = $clientID;
+	else $privateUID = $room['performerID'];
+	$room['privateUID'] = $privateUID;
+
+	
+	//screen
+	$room['screen'] = 'Way2Screen';
+	
+
+	$room['streamBroadcast'] = appStream($userID, $roomID, $options);
+
+	$room['streamUID'] = intval($privateUID);
+	$room['streamPlayback'] = appStream($privateUID, $roomID, $options);
+
+	//$room['actionPrivate'] = !$isPerformer;
+	$room['actionPrivateClose'] = false;
+	$room['privateUID'] = 0;
+
+	$room['actionID'] = 0;
+
+	$room['welcome'] = sprintf('Welcome to public room "%s", user #%s!', $room['name'] , $userID);
+	$room['welcomeImage'] = VW_H5V_URL . 'images/chat.png';
+
+
+
+	if ($isPerformer) //member: performer
+		{
+		$room['welcome'] .= "\n". 'You are room owner (caller).';
+	}
+	else //member: client
+		{
+		$room['welcome'] .= "\n".'You are invited participant (client).';
+		}
+
+	
+	//if ($options['videochatNext']) if (!$isPerformer) $room['next'] = true;
+
+			if ($welcome) $room['welcome'] .= "\n" . $welcome;
+
+			//configure tipping options for clients
+			$room['tips'] = false;
+		if ($options['tips'])
+			if (!$session->broadcaster)
+			{
+				$tipOptions = appTipOptions($options);
+				if (count($tipOptions))
+				{
+					$room['tipOptions'] = $tipOptions;
+					$room['tips'] = true;
+					$room['tipsURL'] = VW_H5V_URL . 'tips/';
+				}
+			}
+
+		//offline snapshot (poster) and video 
+		$room['snapshot'] = VW_H5V_URL . 'images/no-picture.png';
+		$room['videoOffline'] = VW_H5V_URL . 'videos/hamsterad.mp4';;		
+
+		return $room;
+}
+
